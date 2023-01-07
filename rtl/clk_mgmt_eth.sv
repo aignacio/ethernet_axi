a869cbc1f0df0970847afc83ef59c9c10c679949
Anderson Ignacio <anderson@aignacio.com>
2023-01-07 16:44:34 +0000

    Added async reg true on synch. and removed 200MHz clock
    Signed-off-by: Anderson Ignacio <anderson@aignacio.com>

--- rtl/clk_mgmt_eth.sv
+++ rtl/clk_mgmt_eth.sv
@@ -13,7 +13,6 @@ module clk_mgmt_eth(
   input         rst_in,
   output  logic clk_125MHz,
   output  logic clk_90MHz,
-  output  logic clk_200MHz,
   output  logic clk_locked
 );
 
@@ -71,7 +70,7 @@ module clk_mgmt_eth(
     .CLKOUT0B          (),
     .CLKOUT1           (clk_mmcm_out_90MHz),
     .CLKOUT1B          (),
-    .CLKOUT2           (clk_mmcm_out_200MHz),
+    .CLKOUT2           (),
     .CLKOUT2B          (),
     .CLKOUT3           (),
     .CLKOUT3B          (),
@@ -96,11 +95,6 @@ module clk_mgmt_eth(
   IBUFG clk_ibufg_inst(
     .I(clk_in),
     .O(clk_ibufg)
-  );
-
-  BUFG clk_200_bufg_inst(
-    .I(clk_mmcm_out_200MHz),
-    .O(clk_200MHz)
   );
 
 endmodule
